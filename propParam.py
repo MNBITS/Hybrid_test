012d02d0290cbdbe7126d0bdbc7dfefed5176264
MNBITS <80479640+MNBITS@users.noreply.github.com>
2022-02-10 14:56:41 +0530

    Update propParam.py

--- propParam.py
+++ propParam.py
@@ -1,20 +1,36 @@
-
+from math import comb
 from Inputs import UserInputs
 
 grain = UserInputs(10, 10, 10, 60, 30)
 grain.setChamberPressure()
-
+R = 8.31
 
 
 class FuelData():
-    def __init__(self, Go, c_star, exp, density, coeff,thrust_user):
+    def __init__(self, Go, exp, density, coeff, combustionTemp,k):
         self.Go = Go
-        self.c_star = c_star
-        self.mf_dot = Go*grain.chamberP*grain.At/c_star
+        #self.mf_dot = Go*grain.chamberP*grain.At/c_star
         self.coeff = coeff
         self.exp = exp
         self.density = density
-        self.thrust_user=thrust_user
+        self.combustionTemp = combustionTemp
+        self.k = k
+        
+
+    def calculateC_star(self):
+        numerator = self.combustionTemp*R
+        alpha = self.k+1/self.k-1
+        beta = 2/self.k+1
+
+        c_star = pow(numerator/self.k*pow(beta, alpha), 0.5)
+       
+        
+        return c_star
+    def calculate_Cf(self):
+        alpha = self.k+1/self.k-1
+        beta = 2/self.k+1
+        Cf=((2*self.k**2/(self.k-1))*((beta)**(alpha))*(1-(14.7/grain.chamberP)**((self.k-1)/self.k)))**0.5
+        return Cf
        
     
 
