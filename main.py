8d288e355e2455fbfeba9da5d36eba04ada21f9b
MNBITS <80479640+MNBITS@users.noreply.github.com>
2022-02-10 14:57:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,82 +2,103 @@ from Inputs import UserInputs
 from propParam import FuelData
 import numpy as np
 import matplotlib.pyplot as mp
+from random import random
 
-guess_thrust=int(input())
-exp = 0.6
-grain = UserInputs(10, 8, 10, 60, 30)     # Outer dia, Core Dia, length, throat area, step size
-fuel = FuelData(120, 996, exp, 10, 2000,guess_thrust)      # Go, c_star, exponent(burn rate), coefficient, burn rate,fuel_user
+coreDia = 0
+outerDia = 10
+length = 0
+stepSize = 0.
+Thrust = 1000
+Cf = 1
+useBarlow = True
+fos = 2
+maxStress = 0
+Pc = 0
+
+def setChamberPressure(useBarlow, fos):
+    if(useBarlow):
+        Pc = 2*maxStress*(outerDia - coreDia)/(outerDia*fos)
+    
+    else:
+        Pc = 2000
+    return Pc
+
+throatArea = Thrust/Cf*setChamberPressure(20,30)
+
+exp = 0.2
+Go = 120
+a = 0
+burnRate = 0
+density = 0
+coeff = 0
+combustionTemp = 0
+k = 0
+
+grain = UserInputs(outerDia, coreDia, length, throatArea, stepSize)     # Outer dia, Core Dia, length, throat area, step size
+fuel = FuelData(Go, exp, density, coeff, combustionTemp, k)      # Go, c_star, exponent(burn rate), coefficient, burn rate
+
 # Grain Volume pi(D2 - d2)/4*length
-grainVol = np.pi*(pow(grain.outerDia, 2) - pow(grain.coreDia, 2))*0.25*grain.length
+grainVol = np.pi*((pow(grain.outerDia, 2) - pow(grain.coreDia, 2)))*0.25*grain.length
+
+#Port to throat
+Va = np.pi*grain.outerDia*grain.length - grainVol
+V1 = grainVol/Va
+portTothroat = np.pi*grain.outerDia**2*(1-V1)/(4*throatArea)
+
 
 # Grain Mass grainVol*Density
 fuelMass = grainVol*fuel.density
-massEjected = 0
-m_dot  = 56
+
+m_dot = 90
+Isp = 0
+BurnTime = 20
+
+totalSteps = int(BurnTime/stepSize)
+
+#initialize Time array
+time = np.zeros(totalSteps, dtype = float)
+
 stepNum = 0
-Isp  = 10
-
-print(massEjected)
-print(fuelMass)
-
-time = np.zeros(grain.stepSize, dtype = float)
-
-while (stepNum<grain.stepSize):
-    c=1;
+while(stepNum < totalSteps):
     
-    #initiate radius array R(t)
-    radius = np.empty(grain.stepSize, dtype = float)
-    radius[stepNum] = 0
-    print(radius[stepNum])
-
-    m_dot_ox = np.zeros(grain.stepSize, dtype = float)
-    m_dot_f = np.zeros(grain.stepSize, dtype = float)
-    m_dot_tot = np.zeros(grain.stepSize, dtype = float)
-
-    m_dot_ox[stepNum] = fuel.Go*grain.portArea
-
+    #initiate arrays
+    radius = np.empty(totalSteps, dtype = float)
+    m_dot_ox = np.zeros(totalSteps, dtype = float)
+    m_dot_f = np.zeros(totalSteps, dtype = float)
+    m_dot_t = np.zeros(totalSteps, dtype = float)
+    Ph = np.zeros(totalSteps, dtype = float)
+    OF_ratio = np.zeros(grain.stepSize, dtype = float)
+     
+    #Calculate m_dot_t
+    m_dot_t[stepNum] = fuel.Go*Pc*throatArea/(fuel.calculateC_star)
+    
+    #Calculate time
     time[stepNum] = grain.stepSize*stepNum
 
-    alpha = pow(m_dot_ox[stepNum]/np.pi, fuel.exp)
+    #Calculate Ph
+    Ph[stepNum] = [1+0.5*pow(fuel.k*pow(2/fuel.k+1), fuel.k+1/fuel.k-1)]
 
-    rad = pow((fuel.coeff*(2*fuel.exp+1)*alpha*time[stepNum]) + pow(grain.coreDia, 2*fuel.exp+1), 1/(2*fuel.exp + 1))
-    print(alpha)
+    #m_dot_f[stepNum] = 2*np.pi*fuel.density*grain.length*alpha*pow(radius[stepNum], 1-2*fuel.exp)
 
-    m_dot_f[stepNum] = 2*np.pi*fuel.density*grain.length*alpha*pow(radius[stepNum], 1-2*fuel.exp)
 
-    OF_ratio = np.zeros(grain.stepSize, dtype = float)
 
-    #OF_ratio[stepNum] = m_dot_ox[stepNum]/m_dot_f[stepNum]
 
-    m_dot_tot = np.array(grain.stepSize, dtype = float)
 
     #m_dot_tot[stepNum] = m_dot_f[stepNum] + m_dot_ox[stepNum]
 
 
     thrust = np.zeros(grain.stepSize, dtype = float)
-    thrust[stepNum] = m_dot_tot*Isp*fuel.Go
-    if(fuel.thrust_user<1.03*thrust[stepNum]and fuel.thrust_user>0.97*thrust[stepNum]):
-        break
-    elif(fuel.thrust_user<thrust[stepNum]):
-        thrust[stepNum]=thrust[stepNum]*(1-(0.5)**c)
-    else:
-         thrust[stepNum]=thrust[stepNum]*(1+(0.5)**c)
-         
-        
-    
-    c=c+1;
+    thrust[stepNum] = m_dot_t[stepNum]*Isp*fuel.Go*(1 + (-1)**random.uniform(1, 2)*random.random()/33)
 
 
     portArea = np.pi*(pow(grain.outerDia, 2) - pow(4*radius[stepNum], 2))*0.25
     stepNum+=1
     massEjected = m_dot*grain.stepSize*stepNum
-    #fuelMass = np.pi*fuel.density*grain.length*((radius[stepNum]**2) - (grain.coreDia** 2))
+    fuelMass = np.pi*fuel.density*grain.length*(pow(radius[stepNum], 2) - pow(grain.coreDia, 2))
+    burnRate=m_dot_t[stepNum]/((throatArea)*density)
     
     #print(radius[stepNum])
     print("\n")
 
 mp.plot(thrust, time, color = "red")
-
 mp.show()
-
-
